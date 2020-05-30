366a07d7d1ee154fbfc6d37532118199851cfcff
Maicon Slavieiro <maiconslavieiro@outlook.com>
2020-05-30 17:25:36 -0300

    Desafio conceitos do ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data)
+    })
+  }, []);
+
+  const [repositories, setRepository] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+      title: "Desafio conceito ReactJS",
+      url: "https://skylab.rocketseat.com.br/",
+      techs: ["React, ReactJS"]
+    };
+
+    api.post('repositories', repository)
+    .then((response) => {
+      setRepository([...repositories, response.data])
+    }) 
+    .catch((error) => {
+      console.log(error)
+    })  
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    const newRepositorie = repositories;
+    newRepositorie.splice(repositoryIndex, 1);
+    api.delete(`repositories/${id}`)
+    .then(()=> {
+      setRepository([...newRepositorie]);      
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repository => 
+            <li key={repository.id}>
+            {repository.title}
+  
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+          )
+        }
+       
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
